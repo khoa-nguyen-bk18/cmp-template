b9a8397a2994116f9cae6b13370cf061713e40db
khoanguyendang <khoa.nguyen.bk18@gmail.com>
2026-06-01 15:02:53 +0700

    update: random assign category for fake card data

--- domain/src/commonMain/kotlin/com/devindie/cmptemplate/domain/model/browse/BrowseCategory.kt
+++ domain/src/commonMain/kotlin/com/devindie/cmptemplate/domain/model/browse/BrowseCategory.kt
@@ -1,11 +1,7 @@
 package com.devindie.cmptemplate.domain.model.browse
 
 enum class BrowseCategory(val label: String) {
-    All(label = "All"),
-    Pokemon(label = "Pokémon"),
-    Magic(label = "Magic"),
-    Sports(label = "Sports"),
-    ;
+    All(label = "All"), Pokemon(label = "Pokémon"), Magic(label = "Magic"), Sports(label = "Sports");
 
     companion object {
         val filters: List<BrowseCategory> = entries
